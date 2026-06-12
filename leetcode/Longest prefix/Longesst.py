3ad74c224c9ea0385e706eaa39516660a0b0d4ca
Anuoluwa <odebiyianuoluwapo8@gmail.com>
2026-06-12 23:55:27 +0100

    updated

---
+++ leetcode/Longest prefix/Longesst.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
+
+
+    strs = ["mistake", "misappreciation", "misunderstanding", "misconception", "misuse"]  
+     
